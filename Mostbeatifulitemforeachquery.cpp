d2d30483d19a17abde5861caf677d59ec5c971f3
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2024-12-05 14:31:49 +0100

    mods

--- Mostbeatifulitemforeachquery.cpp
+++ Mostbeatifulitemforeachquery.cpp
@@ -1,9 +1,33 @@
-    // LeetCode starter code
-    #include <iostream>
     #include <vector>
+    #include <algorithm>
+    #include <map>
+#include <climits>
+    
     using namespace std;
-
-    int main() {
-        // Your code here
-        return 0;
-    }
+    
+class Solution {
+    public:
+        vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+            vector<pair<int, int>> nums;
+            vector<int> ans;
+            sort(items.begin(), items.end());
+            
+            int mx = 0;
+            for(const auto& item : items) {
+                mx = max(mx, item[1]);
+                nums.push_back({item[0], mx});
+            }
+            
+            for(int x : queries) {
+                auto it = upper_bound(nums.begin(), nums.end(), make_pair(x, INT_MAX));
+                if(it == nums.begin()) {
+                    ans.push_back(0);
+                } else {
+                    --it;
+                    ans.push_back(it->second);
+                }
+            }
+            
+            return ans;
+        }
+};
