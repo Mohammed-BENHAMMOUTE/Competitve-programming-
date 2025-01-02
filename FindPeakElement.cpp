e2321f43692d6fa4cb5b89b32915763185ebb00a
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2025-01-02 19:50:09 +0100

    feat: enhance findPeakElement function to handle edge cases for first and last elements

--- FindPeakElement.cpp
+++ FindPeakElement.cpp
@@ -11,7 +11,10 @@ using namespace std;
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
+
         int n = nums.size();
+        if( n >1 && nums[0] > nums[1]) return 0;
+        if( n >1 && nums[n-1] > nums[n-2]) return n-1;
         int l = 1; int r = n-2;
         int mid  = -1;
         while(l <= r) {
