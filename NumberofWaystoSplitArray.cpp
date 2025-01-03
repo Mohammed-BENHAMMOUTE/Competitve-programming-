e4bd7cae84bc7a53e7255093f4aaa2b556a993f7
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2025-01-03 09:40:00 +0100

    feat: add solution for Number of Ways to Split Array problem

---
+++ NumberofWaystoSplitArray.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define endl "\n"
+using namespace std;
+
+class Solution {
+public:
+    int waysToSplitArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<long long> pre(n+1, 0);
+        vector<long long> suf(n+1, 0);
+        for(int i = 0; i < n; i++) {
+            pre[i+1] = pre[i] + nums[i];
+        }
+        for(int i = n-1; i >= 0; i--) {
+            suf[i] = suf[i+1] + nums[i];
+        }
+        int count = 0;
+        for(int i = 0; i < n-1; i++) {
+            if(pre[i+1] >= suf[i+1]) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
