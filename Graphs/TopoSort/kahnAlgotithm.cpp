875b3c900951d79be021a61851f0e14c3d9e15db
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2025-01-28 22:10:47 +0100

    feat: refactor Kahn's algorithm implementation for topological sorting in C++

--- Graphs/TopoSort/kahnAlgotithm.cpp
+++ Graphs/TopoSort/kahnAlgotithm.cpp
@@ -1,104 +1,46 @@
-/*
- * Author: Mohammed BENHAMMOUTE
- * Created: 2025-01-20 11:04:02
- */
 #include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
 using namespace std;
 
-// Debug macros
-#define debug(x) cerr << #x << " = " << x << endl
-#define debugv(v) cerr<<#v<<" = ";for(auto e:v)cerr<<e<<" ";cerr<<endl
-#define debugm(m) cerr<<#m<<endl;for(auto e:m)cerr<<e.first<<" "<<e.second<<endl
-#define debuga(a,n) cerr<<#a<<" = ";for(int i=0;i<n;i++)cerr<<a[i]<<" ";cerr<<endl
-#define debugma(a,n,m) cerr<<#a<<":\n";for(int i=0;i<n;i++){for(int j=0;j<m;j++)cerr<<a[i][j]<<" ";cerr<<endl;}
-
-// Type definitions
-#define ll long long
-#define ld long double
-#define ull unsigned long long
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define vi vector<int>
-#define vl vector<ll>
-#define vvi vector<vi>
-#define vvl vector<vl>
-#define vpii vector<pii>
-#define vpll vector<pll>
-
-// Shortcuts
-#define pb push_back
-#define mp make_pair
-#define F first
-#define S second
-#define all(x) x.begin(),x.end()
-#define rall(x) x.rbegin(),x.rend()
-#define sz(x) (int)x.size()
-#define rep(i,a,b) for(int i=a;i<b;i++)
-#define per(i,a,b) for(int i=b-1;i>=a;i--)
-
-// Constants
-const int MOD = 1e9+7;
-const ll INF = 1e18;
-const ld PI = acos(-1.0);
-const int dx[8] = {1,-1,0,0,1,1,-1,-1};
-const int dy[8] = {0,0,1,-1,1,-1,1,-1};
-
-// Utility functions
-template<typename T> inline T gcd(T a, T b) {return b ? gcd(b, a%b) : a;}
-template<typename T> inline T lcm(T a, T b) {return a * (b/gcd(a,b));}
-template<typename T> inline T mod(T a, T b) {return (a%b + b)%b;}
-template<typename T> inline T modpow(T a, T b, T m) {T r=1; while(b) {if(b&1) r=r*a%m; a=a*a%m; b>>=1;} return r;}
-
-// Vector operations
-template<typename T> void amax(T &a, T b) {if(b > a) a = b;}
-template<typename T> void amin(T &a, T b) {if(b < a) a = b;}
-template<typename T> void print(vector<T> &v) {for(auto x:v) cout<<x<<" "; cout<<endl;}
-template<typename T> void print2d(vector<vector<T>> &v) {for(auto x:v) {for(auto y:x) cout<<y<<" "; cout<<endl;}}
-
-// Fast I/O
-void fastIO() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    cout << fixed << setprecision(10);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-        freopen("error.txt", "w", stderr);
-    #endif
-}
-
 class Solution {
-public: 
-    vector<int> topologicalSort(vector<vector<int>> &adj){
+public:
+    vector<int> topologicalSort(vector<vector<int>> &adj) {
         int n = adj.size();
-        priority_queue<int, vector<int> , greater<int>> pq;
-        vector<bool> visited(n, false);
-        vector<int> inDegree(n, 0);
-        for(int i =0 ; i < n ; i++) {
-            for(int j = 0; j < adj[i].size() ; j++){
-                inDegree[adj[i][j]]++;
+        vector<int> result;  
+        priority_queue<int, vector<int>, greater<int>> pq;  
+        vector<int> indegree(n, 0);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < adj[i].size(); j++) {
+                indegree[adj[i][j]]++;
             }
-        };
-        for(int i =0 ; i < n ;i++){
-            if(inDegree[i] == 0) pq.push(i);
         }
-        for(int i =0 ; i < n ; i++){
-            inDegree[i] = adj[i].size();
+        for(int i = 0; i < n; i++) {
+            if(indegree[i] == 0) {
+                pq.push(i);
+            }
         }
-        vector<int> ans;
-        while(!pq.empty()){
+        while(!pq.empty()) {
             int current = pq.top();
             pq.pop();
-            
-            for(int i = 0; i < adj[current].size() ; i++){
-                adj[current][i]--;
-                if(inDegree[adj[current][i]] == 0){
-                    pq.push(adj);
+            result.push_back(current);
+
+            for(int next : adj[current]) {
+                indegree[next]--;
+                if(indegree[next] == 0) {
+                    pq.push(next);
                 }
             }
         }
+        if(result.size() != n) {
+            return {}; 
+        }
+
+        return result;
     }
-private:
-    void KahnAlgorithm(int s , vector<int> &adj , vector<int> &visited , ){}
-}
+};
