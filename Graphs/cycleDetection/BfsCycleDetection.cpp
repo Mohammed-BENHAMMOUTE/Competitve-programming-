88a07342c4e7515662d2f0cb72e8cd1361f30c9d
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2025-01-29 01:13:43 +0100

    feat: implement cycle detection in directed graphs using BFS

---
+++ Graphs/cycleDetection/BfsCycleDetection.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
+using namespace std;
+
+class Solution {
+public: 
+    bool isCyclic(int V, vector<vector<int>> adj) {
+        vector<int> indegree(V , 0);
+        for(int i =0 ; i < V ; i++) 
+        {
+            for(int x : adj[i])
+            {
+                indegree[x]++;
+            }
+        }
+        queue<int> q;
+        for(int i = 0; i < V ; i++) {
+            if(indegree[i] == 0 ){
+                q.push(i);
+            }
+        };
+
+        int cnt  = 0;
+        while(!q.empty()){
+            int current = q.front();q.pop();cnt++;
+            for(int x : adj[current]){
+                indegree[x]--;
+                if(indegree[x] == 0){
+                    q.push(x);
+                }
+            }
+        }
+        return cnt != V;
+    };
+};
