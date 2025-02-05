567044c410d1497f8bd754752a0be66187afbcb7
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2025-02-05 01:42:19 +0100

    refactor: optimize next permutation algorithm for clarity and efficiency

--- NextPermutation.cpp
+++ NextPermutation.cpp
@@ -11,23 +11,21 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int i = n - 2;
-
-        // Step 1: Find the first decreasing element from the end
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-        if (i >= 0) 
+        // we need the rightmost element that is smaller than its next element
+        int pivot = n-2;
+        while(pivot >= 0 && nums[pivot] >= nums[pivot +1])
         {
-            // Step 2: Find the element just larger than nums[i] and swap them
-            int j = n - 1;
-            while (j >= 0 && nums[j] <= nums[i]) 
-            {
-                j--;
-            }
-            swap(nums[i], nums[j]);
+            pivot--;
         }
-        // Step 3: Reverse the elements after the first decreasing element
-        reverse(nums.begin() + i + 1, nums.end());
+        if(pivot >=0){
+            int swappedElement = n-1;
+            while (swappedElement >= 0 && nums[pivot] >= nums[swappedElement])
+            {
+                swappedElement--;
+            }
+            swap(nums[pivot] , nums[swappedElement]);
+        }
+        reverse(nums.begin()+pivot +1 , nums.end());
+    
     }
 };
