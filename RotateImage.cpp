004dfd3695e40fee8246fe3a536c875711f87267
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2025-02-05 14:48:38 +0100

    feat: implement Rotate Image algorithm and clean up Set Matrix Zeroes code

---
+++ RotateImage.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i = 0 ; i < n ; i++)
+        {
+            for(int j = i+1; j < n ; j++){
+                swap(matrix[i][j] , matrix[j][i]);
+            }
+        }
+        for(auto  row : matrix){
+            reverse(row.begin() , row.end());
+        }
+    }
+};
--- SetMatrixZeroes.cpp
+++ SetMatrixZeroes.cpp
@@ -11,8 +11,6 @@ public:
         
         unordered_set<int> rows;
         unordered_set<int> columns;
-        
-        // First pass: record the rows and columns that need to be zeroed
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 0) {
@@ -21,13 +19,10 @@ public:
                 }
             }
         }
-        
-        // Second pass: set the recorded rows to zero
         for (int x : rows) {
             fill(matrix[x].begin(), matrix[x].end(), 0);
         }
-        
-        // Third pass: set the recorded columns to zero
+
         for (int y : columns) {
             for (int i = 0; i < n; i++) {
                 matrix[i][y] = 0;
