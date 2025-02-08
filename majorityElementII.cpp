245f89e1b68ca6db9a833ddd7f60c7c79c2e5733
Mohammed-BENHAMMOUTE <mohammedbenhammoute@gmail.com>
2025-02-08 16:18:10 +0100

    feat: implement Majority Element II algorithm

---
+++ majorityElementII.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+using namespace std;
+
+vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int candidate1 = 0, count1 = 0;
+        int candidate2 = 0, count2 = 0;
+
+        for (const int& num : nums) {
+            if (candidate1 == num) {
+                count1++;
+            } else if (candidate2 == num) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = count2 = 0;
+        for (const int& num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+
+        vector<int> ans;
+        if (count1 > n / 3) ans.push_back(candidate1);
+        if (count2 > n / 3) ans.push_back(candidate2);
+        return ans;
+}
+
+
